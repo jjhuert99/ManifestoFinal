48a329b25791e36815e4110ebb2f88b0f6a7277d
jjhuert99 <jahuert99@gmail.com>
2021-11-18 13:38:38 -0600

    finished

--- app/src/main/java/com/example/manifestofinal/SplashScreenActivity.kt
+++ app/src/main/java/com/example/manifestofinal/SplashScreenActivity.kt
@@ -8,18 +8,19 @@ import android.widget.TextView
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_splash_screen)
 
         findViewById<TextView>(R.id.splash_m).alpha = 0f
         findViewById<TextView>(R.id.splash_title).alpha = 0f
 
-        findViewById<TextView>(R.id.splash_m).animate().setDuration(1100).alpha(1f).withEndAction {
+        findViewById<TextView>(R.id.splash_m).animate().setDuration(800).alpha(1f).withEndAction {
             val i = Intent(this, MainActivity::class.java)
             startActivity(i)
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
             finish()
         }
-        findViewById<TextView>(R.id.splash_title).animate().setDuration(1100).alpha(1f).withEndAction {
+        findViewById<TextView>(R.id.splash_title).animate().setDuration(800).alpha(1f).withEndAction {
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
         }
     }
